24e11dc3db63a061c35a61a5b5999a74600453e1
chris <cryptoskillz@chriss-MacBook-Air.local>
2023-10-14 14:21:22 +0700

    fixed asset dir

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -1,25 +1,13 @@
-var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
 };
 
-window.TrelloPowerUp.initialize({
-  'card-buttons': function (t, opts) {
+TrelloPowerUp.initialize({
+  'card-buttons': function(t, options){
     return [{
-      // usually you will provide a callback function to be run on button click
-      // we recommend that you use a popup on click generally
-      icon: GRAY_ICON, // don't use a colored icon here
-      text: 'Open Popup',
-      callback: onBtnClick,
-      condition: 'edit'
-    }, {
-      // but of course, you could also just kick off to a url if that's your thing
-      icon: GRAY_ICON,
-      text: 'Just a URL',
-      condition: 'always',
-      url: 'https://developer.atlassian.com/cloud/trello',
-      target: 'Trello Developer Site' // optional target for above url
+      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+      text: 'Estimate Size',
     }];
-  }
+  },
 });
