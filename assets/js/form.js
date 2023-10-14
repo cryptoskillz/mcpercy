a8b99de01cd3d2722c8c810380b90e6394e171f2
chris <cryptoskillz@chriss-MacBook-Air.local>
2023-10-14 17:56:36 +0700

    added form

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,7 +4,8 @@ window.trelloform.addEventListener("submit", function(event) {
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
     var commentText = 'This is a comment to update the activity log.';
-
+  var cardId = t.getContext().card;
+console.log(cardId.id)
     return t
         .set("card", "shared", "comments", "yay")
         .then(function() {
@@ -46,6 +47,11 @@ window.trelloform.addEventListener("submit", function(event) {
 t.render(function() {
     t.sizeTo("#trelloform").done();
 });
+
+
+curl --request POST \
+  --url 'https://api.trello.com/1/cards/aBFTnUXw/actions/comments?text={text}&key=c86b743cedafad2bf66b12783fa21a36&token=ATTA2335ae7825ebbc5f622cfb249cd57b8e8df58cf251b53f0fb0611f6f0916ba4a234FB042' \
+  --header 'Accept: application/json'
 
 //get the data to check
 //curl --request GET --url 'https://api.trello.com/1/cards/aBFTnUXw/pluginData?key=c86b743cedafad2bf66b12783fa21a36&token=ATTA2335ae7825ebbc5f622cfb249cd57b8e8df58cf251b53f0fb0611f6f0916ba4a234FB042'
