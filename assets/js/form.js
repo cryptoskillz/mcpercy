0deae9efbeeb0ede0df4fdaa3d16f0ffa38d5373
chris <cryptoskillz@chriss-MacBook-Air.local>
2023-10-14 17:24:51 +0700

    added form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,36 +1,36 @@
 var t = TrelloPowerUp.iframe();
 
-window.trelloform.addEventListener("submit", function (event) {
-  // Stop the browser trying to submit the form itself.
-  event.preventDefault();
-var commentText = 'This is a comment to update the activity log.';
-  
+window.trelloform.addEventListener("submit", function(event) {
+    // Stop the browser trying to submit the form itself.
+    event.preventDefault();
+     var commentText = 'This is a comment to update the activity log.';
   var cardId = t.getContext().card;
 
-  Trello.post(`/1/cards/${cardId}/actions/comments`, {
-    text: commentText,
-    key: 'c86b743cedafad2bf66b12783fa21a36',
-    token: 'ATTA2335ae7825ebbc5f622cfb249cd57b8e8df58cf251b53f0fb0611f6f0916ba4a234FB042',
-  })
-  .then(function () {
-    console.log('Comment added successfully');
-  })
-  .catch(function (error) {
-    console.error('Error adding comment:', error);
-  });
-  //return t.closePopup();
-  /*
-  return t
-    .set("card", "shared", "trelloform", "yay")
-    .then(function () {
-      t.closePopup();
+  t
+    .card('id', 'name')  // Add any other fields you need
+    .then(function(card) {
+      return t
+        .set(card.id, 'shared', 'comment', commentText)
+        .then(function () {
+          console.log('Comment added successfully');
+        })
+        .catch(function (error) {
+          console.error('Error adding comment:', error);
+        });
     });
-    )*/
+    //return t.closePopup();
+    /*
+    return t
+      .set("card", "shared", "trelloform", "yay")
+      .then(function () {
+        t.closePopup();
+      });
+      )*/
 });
 
 
-t.render(function () {
-  t.sizeTo("#trelloform").done();
+t.render(function() {
+    t.sizeTo("#trelloform").done();
 });
 
 //get the data to check
